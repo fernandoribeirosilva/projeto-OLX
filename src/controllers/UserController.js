ca90c5641ea60f5e5ea7d478b3ff0db39e3c87a6
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-13 10:27:10 -0300

    implementa busca dos estados

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,16 @@
+const State = require('../models/State');
+
 module.exports = {
    getStates: async (req, res) => {
+      try {
+         const states = await State.find();
 
+         if (!states) throw new Error('States not found');
+
+         return res.json({ states });
+      } catch (error) {
+         return res.json({ msg: 'Não tem estados cadastrados!' });
+      }
    },
 
    info: async (req, res) => {
