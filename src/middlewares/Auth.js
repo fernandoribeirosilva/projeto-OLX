86d686ffd0601bda67d2c40970d6d9f90138c14a
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-15 10:06:49 -0300

    refatorado o middleware Auth

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -2,16 +2,13 @@ const User = require('../models/User');
 
 module.exports = {
    private: async (req, res, next) => {
-      if (!req.query.token && !req.body.token) return res.status(401).json({ notAllowed: true });
-
       const token = req.query.token || req.body.token;
 
-      if (token === '') return res.status(401).json({ notAllowed: true });
+      if (token) {
+         const user = await User.findOne({ token });
+         if (user) return next();
+      }
 
-      const user = await User.findOne({ token });
-
-      if (!user) return res.status(401).json({ notAllowed: true });
-
-      next();
+      res.json({ notallowed: true });
    }
 }
