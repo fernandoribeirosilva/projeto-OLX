448aa790fb3d0c2452ab8c108ca92f08c7dbdf44
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-15 10:03:47 -0300

    implementado o validator do signup

--- src/controllers/AuthControler.js
+++ src/controllers/AuthControler.js
@@ -1,9 +1,20 @@
+const { validationResult, matchedData } = require('express-validator');
+
 module.exports = {
    signin: async (req, res) => {
 
    },
 
    signup: async (req, res) => {
+      const erros = validationResult(req);
 
+      if (!erros.isEmpty()) {
+         res.status(400).json({ error: erros.mapped() });
+         return;
+      }
+
+      const data = matchedData(req);
+
+      res.status(200).json({ ok: true, data });
    },
 }
