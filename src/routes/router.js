de995cfdf695a43c2615837376a93f5d9b999668
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-17 13:24:33 -0300

    implementado o UserValidator.editAction

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const Auth = require('../middlewares/Auth');
 
 const AuthValidator = require('../validators/AuthValidator');
+const UserValidator = require('../validators/UserValidator');
 
-const AuthController = require('../controllers/AuthControler');
+const AuthController = require('../controllers/AuthController');
 const UserController = require('../controllers/UserController');
 const AdsController = require('../controllers/AdsController');
 
@@ -19,7 +20,7 @@ router.post('/user/signin', AuthValidator.signin, AuthController.signin);
 router.post('/user/signup', AuthValidator.signup, AuthController.signup);
 
 router.get('/user/me', Auth.private, UserController.info);
-router.put('/user/me', Auth.private, UserController.editAction);
+router.put('/user/me', UserValidator.editAction, Auth.private, UserController.editAction);
 
 router.get('/categories', AdsController.getCategories);
 
