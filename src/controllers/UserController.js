6490442f2b0c273724aeb607ab5be898b092631e
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-17 13:20:20 -0300

    implementado função de fazer o update de um usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,3 +1,7 @@
+const { validationResult, matchedData } = require('express-validator');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const State = require('../models/State');
 const User = require('../models/User');
 const Category = require('../models/Category');
@@ -38,7 +42,50 @@ module.exports = {
    },
 
    editAction: async (req, res) => {
+      const erros = validationResult(req);
 
+      if (!erros.isEmpty()) {
+         res.status(400).json({ error: erros.mapped() });
+         return;
+      }
+      const data = matchedData(req);
+
+      let updates = {};
+
+
+      if (data.name) updates.name = data.name;
+
+      if (data.email) {
+         const emailCheck = await User.findOne({ email: data.email });
+         if (emailCheck) {
+            res.json({ error: 'E-mail já existe.' });
+            return;
+         }
+         updates.email = data.email;
+      }
+
+      if (data.state) {
+         if (mongoose.Types.ObjectId.isValid(data.state)) {
+            const stateCheck = await State.findById(data.state);
+            if (!stateCheck) {
+               res.json({ error: 'Estado não existe.' });
+               return;
+            }
+            updates.state = data.state;
+         } else {
+            res.json({
+               error: { state: { msg: 'Código de estado inválido.' } }
+            });
+            return;
+         }
+      }
+
+      if (data.password) {
+         updates.passwordHash = await bcrypt.hash(data.password, 10);
+      }
+
+      await User.findOneAndUpdate({ token: data.token }, updates, { new: true });
+
+      res.json({});
    },
-
 }
